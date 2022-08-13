bb8e76c404d60fb90c1404846f351135852cb6e5
zeyno12 <zeynabbagirova1@gmail.com>
2022-08-13 13:31:48 +0400

    finiw

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// burger
+const burger=document.querySelector(".burger");
+if(burger){
+  burger.addEventListener("click", function () {
+      const mobilnav = document.querySelector(".nav_list");
+      if (mobilnav.style.transform == "translateX(-500px)") {
+        mobilnav.style.transform = "translateX(0px)";
+      }
+      else  {
+        mobilnav.style.transform = "translateX(-500px)";
+      }
+    })
+}
